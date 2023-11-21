d18dc0feb9a3c57c01f047ee0b7df914a407db69
Felipe Gui <fguiengenharia@gmail.com>
2023-11-20 22:55:18 -0300

    Conceito de polimorfismo

--- java_acenelio/src/inheritanceandpolymorphism/Program.java
+++ java_acenelio/src/inheritanceandpolymorphism/Program.java
@@ -4,18 +4,16 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Account acc = new Account(1001, "Alex", 1000.0);
-		acc.withdraw(200.0);
+		Account acc = new Account(1020, "Alex", 1000.0);
+		Account acc2 = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
 		
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-		acc2.withdraw(200.0);
-		
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-		acc3.withdraw(200.0);
+		//Polymorphism
+		//Same operation of the same type but with different behaviors
+		acc.withdraw(50.0);
+		acc2.withdraw(50.0);
 		
 		System.out.println("Conta 1: " + acc.getBalance());
 		System.out.println("Conta 2: " + acc2.getBalance());
-		System.out.println("Conta 3: " + acc3.getBalance());
 	}
 
 }
