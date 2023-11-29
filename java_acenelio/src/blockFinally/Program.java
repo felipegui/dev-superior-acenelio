8bb051a4fcae50fb9bf93886583dc7992f6d9eda
Felipe Gui <fguiengenharia@gmail.com>
2023-11-29 12:01:34 -0300

    Usando bloco finally

---
+++ java_acenelio/src/blockFinally/Program.java
@@ -0,0 +1,33 @@
+package blockFinally;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		File file = new File("C:\\temp\\in.txt");
+
+		Scanner sc = null;
+
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		}
+		//O bloco finally executa independente de try e catch
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed");
+		}
+
+	}
+
+}
